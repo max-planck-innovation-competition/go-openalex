66f77e7b6a901be4a3bef3ef713fef0f03bfe67b
Seb <git@erhardt.net>
2023-10-16 15:31:08 +0200

    refactor functions and make them more granular

--- pkg/openalex/process_directory.go
+++ pkg/openalex/process_directory.go
@@ -28,32 +28,62 @@ func visit(files *[]string) filepath.WalkFunc {
 func ProcessDirectory(directoryPath string, fnEntityHandler ParsedEntityLineHandler, fnMergedIdHandler MergedIdRecordHandler) (err error) {
 	logger := slog.With("directoryPath", directoryPath)
 	logger.Info("Start reading directory")
-	var filePaths []string // stores the filePath paths of all the files in the directory
+	// get the files
+	filePaths, err := GetFiles(directoryPath)
+	if err != nil {
+		logger.With("err", err).Error("error while reading the directory")
+		return
+	}
+	// process the files
+	err = ProcessFiles(filePaths, fnEntityHandler, fnMergedIdHandler)
+	if err != nil {
+		logger.With("err", err).Error("error while processing the files")
+		return
+	}
+	logger.Info("Finished reading directory")
+	return
+}
 
+// GetFiles returns a list of files in a directory
+func GetFiles(directoryPath string) (filePaths []string, err error) {
+	logger := slog.With("directoryPath", directoryPath)
+	logger.Info("Start listing directory")
 	// walk over the files in the directory
 	err = filepath.Walk(directoryPath, visit(&filePaths))
 	if err != nil {
 		logger.With("err", err).Error("error while walking the directory")
-		return err
+		return
 	}
+	logger.Info("Finished listing directory")
+	return
+}
+
+// ProcessFiles parses the files and processes them
+func ProcessFiles(filePaths []string, fnEntityHandler ParsedEntityLineHandler, fnMergedIdHandler MergedIdRecordHandler) (err error) {
+	logger := slog.With("method", "ProcessFiles")
 	// process all files
 	for _, filePath := range filePaths {
 		if strings.Contains(filePath, "merged_ids") {
 			// handle merged ids file
 			errFile := ParseMergedIDsFile(filePath, fnMergedIdHandler)
 			if errFile != nil {
-				logger.With("err", errFile).Error("error while parsing the merged id file")
+				logger.
+					With("err", errFile).
+					With("filePath", filePath).
+					Error("error while parsing the merged id file")
 				return errFile
 			}
 		} else {
 			// handle other files
 			errFile := ParseFile(filePath, fnEntityHandler)
 			if errFile != nil {
-				logger.With("err", errFile).Error("error while parsing the file")
+				logger.
+					With("err", errFile).
+					With("filePath", filePath).
+					Error("error while parsing the file")
 				return errFile
 			}
 		}
 	}
-	logger.Info("Finished reading directory")
 	return
 }
