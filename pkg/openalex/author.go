bf6a3c50a65c229d57312a37aba1f3ef97e7173e
Seb <git@erhardt.net>
2023-12-22 16:21:59 +0100

    remove nesting

--- pkg/openalex/author.go
+++ pkg/openalex/author.go
@@ -2,47 +2,55 @@ package openalex
 
 // Author is a struct that represents the data of an author of OpenAlex
 type Author struct {
-	ID           string `json:"id"`
-	CitedByCount int    `json:"cited_by_count"`
-	CountsByYear []struct {
-		Year         int `json:"year"`
-		WorksCount   int `json:"works_count"`
-		CitedByCount int `json:"cited_by_count"`
-	} `json:"counts_by_year"`
-	CreatedDate             string   `json:"created_date"`
-	DisplayName             string   `json:"display_name"`
-	DisplayNameAlternatives []string `json:"display_name_alternatives"`
-	Ids                     struct {
-		Openalex  string `json:"openalex"`
-		Orcid     string `json:"orcid"`
-		Scopus    string `json:"scopus"`
-		Wikipedia string `json:"wikipedia"`
-	} `json:"ids"`
-	LastKnownInstitution struct {
-		CountryCode string `json:"country_code"`
-		DisplayName string `json:"display_name"`
-		ID          string `json:"id"`
-		Ror         string `json:"ror"`
-		Type        string `json:"type"`
-	} `json:"last_known_institution"`
-	MostCitedWork string `json:"most_cited_work"`
-	Orcid         string `json:"orcid"`
-	SummaryStats  struct {
-		CitedByCount2yr  int     `json:"2yr_cited_by_count"`
-		HIndex2yr        int     `json:"2yr_h_index"`
-		I10Index2yr      int     `json:"2yr_i10_index"`
-		MeanCitedness2yr float64 `json:"2yr_mean_citedness"`
-		WorksCount2yr    int     `json:"2yr_works_count"`
-		CitedByCount     int     `json:"cited_by_count"`
-		HIndex           int     `json:"h_index"`
-		I10Index         int     `json:"i10_index"`
-		OaPercent        float64 `json:"oa_percent"`
-		WorksCount       int     `json:"works_count"`
-	} `json:"summary_stats"`
-	UpdatedDate string           `json:"updated_date"`
-	WorksAPIURL string           `json:"works_api_url"`
-	WorksCount  int              `json:"works_count"`
-	XConcepts   []AuthorXConcept `json:"x_concepts"`
+	ID                      string                     `json:"id"`
+	CitedByCount            int                        `json:"cited_by_count"`
+	CountsByYear            []AuthorCountsByYear       `json:"counts_by_year"`
+	CreatedDate             string                     `json:"created_date"`
+	DisplayName             string                     `json:"display_name"`
+	DisplayNameAlternatives []string                   `json:"display_name_alternatives"`
+	Ids                     AuthorIDs                  `json:"ids"`
+	LastKnownInstitution    AuthorLastKnownInstitution `json:"last_known_institution"`
+	MostCitedWork           string                     `json:"most_cited_work"`
+	Orcid                   string                     `json:"orcid"`
+	SummaryStats            AuthorSummaryStats         `json:"summary_stats"`
+	UpdatedDate             string                     `json:"updated_date"`
+	WorksAPIURL             string                     `json:"works_api_url"`
+	WorksCount              int                        `json:"works_count"`
+	XConcepts               []AuthorXConcept           `json:"x_concepts"`
+}
+
+type AuthorCountsByYear struct {
+	Year         int `json:"year"`
+	WorksCount   int `json:"works_count"`
+	CitedByCount int `json:"cited_by_count"`
+}
+
+type AuthorIDs struct {
+	Openalex  string `json:"openalex"`
+	Orcid     string `json:"orcid"`
+	Scopus    string `json:"scopus"`
+	Wikipedia string `json:"wikipedia"`
+}
+
+type AuthorLastKnownInstitution struct {
+	CountryCode string `json:"country_code"`
+	DisplayName string `json:"display_name"`
+	ID          string `json:"id"`
+	Ror         string `json:"ror"`
+	Type        string `json:"type"`
+}
+
+type AuthorSummaryStats struct {
+	CitedByCount2yr  int     `json:"2yr_cited_by_count"`
+	HIndex2yr        int     `json:"2yr_h_index"`
+	I10Index2yr      int     `json:"2yr_i10_index"`
+	MeanCitedness2yr float64 `json:"2yr_mean_citedness"`
+	WorksCount2yr    int     `json:"2yr_works_count"`
+	CitedByCount     int     `json:"cited_by_count"`
+	HIndex           int     `json:"h_index"`
+	I10Index         int     `json:"i10_index"`
+	OaPercent        float64 `json:"oa_percent"`
+	WorksCount       int     `json:"works_count"`
 }
 
 type AuthorXConcept struct {
